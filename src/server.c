974470e0bfbb415e50c7dfd66f448da28751c68b
Tomasz Pewiński <pewniak747@gmail.com>
2013-02-13 21:44:13 +0100

    initializing log semaphore

--- src/server.c
+++ src/server.c
@@ -4,25 +4,38 @@
 
 #include "common.h"
 
-int repo_sem_init(int *repo_sem) {
-  int _repo_sem = semget(SEM_REPO, 1, 0666 | IPC_CREAT | IPC_EXCL);
-  if(-1 != _repo_sem) {
-    short semval[1] = { 0 };
-    semctl(_repo_sem, 0, SETVAL, semval);
+int log_sem_init() {
+  int log_sem = semget(SEM_LOG, 1, 0666 | IPC_CREAT | IPC_EXCL);
+  if(-1 != log_sem) {
+    short semval[1] = { 1 };
+    semctl(log_sem, 0, SETVAL, semval);
   }
   else {
-    _repo_sem = semget(SEM_REPO, 1, 0666);
+    log_sem = semget(SEM_LOG, 1, 0666);
   }
-  return _repo_sem;
+  return log_sem;
+}
+
+int repo_sem_init() {
+  int repo_sem = semget(SEM_REPO, 1, 0666 | IPC_CREAT | IPC_EXCL);
+  if(-1 != repo_sem) {
+    short semval[1] = { 0 };
+    semctl(repo_sem, 0, SETVAL, semval);
+  }
+  else {
+    repo_sem = semget(SEM_REPO, 1, 0666);
+  }
+  return repo_sem;
 }
 
 void repo_init(int *repo_id, int *repo_sem, int *log_sem) {
-  *repo_sem = repo_sem_init(repo_sem);
+  *repo_sem = repo_sem_init();
+  *log_sem  = log_sem_init();
 }
 
 int main(int argc, char *argv[]) {
   int repo_id, repo_sem, log_sem;
   repo_init(&repo_id, &repo_sem, &log_sem);
-  printf("%d\n", repo_sem);
+  printf("repo sem: %d\nlog sem: %d\n", repo_sem, log_sem);
   return 0;
 }
