e2ff5d00a99d2d5debec6506516963cd7991b8f2
Tomasz Pewiński <pewniak747@gmail.com>
2013-02-14 20:32:16 +0100

    basic registering of a server

--- src/server.c
+++ src/server.c
@@ -2,6 +2,7 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
+#include <unistd.h>
 
 #include "common.h"
 
@@ -27,6 +28,17 @@ void repo_access_start(int repo_sem) {
 
 void repo_access_stop(int repo_sem) {
   sem_raise(repo_sem);
+}
+
+void repo_server_register(REPO *repo, int repo_sem) {
+  repo_access_start(repo_sem);
+  SERVER server;
+  server.client_msgid = getpid();
+  server.server_msgid = 0;
+  server.clients = 0;
+  repo->servers[0] = server;
+  repo->active_servers++;
+  repo_access_stop(repo_sem);
 }
 
 REPO *repo_get(int repo_id, int repo_sem) {
@@ -92,7 +104,9 @@ int main(int argc, char *argv[]) {
   int repo_id, repo_sem, log_sem;
   repo_init(&repo_id, &repo_sem, &log_sem);
   printf("repo sem: %d\nlog sem: %d\nrepo_id: %d\n", repo_sem, log_sem, repo_id);
+  sem_raise(repo_sem);
   REPO *repo = repo_get(repo_id, repo_sem);
+  repo_server_register(repo, repo_sem);
   printf("active servers: %d\n", repo->active_servers);
   repo_release(repo, repo_sem);
   return 0;
