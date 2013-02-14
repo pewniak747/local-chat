168905e5c047d294254da25e433e7088b12d7557
Tomasz Pewiński <pewniak747@gmail.com>
2013-02-14 20:06:24 +0100

    getting and releasng repo

--- src/server.c
+++ src/server.c
@@ -21,6 +21,27 @@ void sem_raise(int sem_id) {
   semop(sem_id, &buf, 1);
 }
 
+void repo_access_start(int repo_sem) {
+  sem_lower(repo_sem);
+}
+
+void repo_access_stop(int repo_sem) {
+  sem_raise(repo_sem);
+}
+
+REPO *repo_get(int repo_id, int repo_sem) {
+  repo_access_start(repo_sem);
+  REPO *mem = shmat(repo_id, NULL, 0);
+  repo_access_stop(repo_sem);
+  return mem;
+}
+
+void repo_release(REPO *repo, int repo_sem) {
+  repo_access_start(repo_sem);
+  shmdt(repo);
+  repo_access_stop(repo_sem);
+}
+
 int log_sem_init() {
   int log_sem = semget(SEM_LOG, 1, 0666 | IPC_CREAT | IPC_EXCL);
   if(-1 != log_sem) {
@@ -73,5 +94,8 @@ int main(int argc, char *argv[]) {
   int repo_id, repo_sem, log_sem;
   repo_init(&repo_id, &repo_sem, &log_sem);
   printf("repo sem: %d\nlog sem: %d\nrepo_id: %d\n", repo_sem, log_sem, repo_id);
+  REPO *repo = repo_get(repo_id, repo_sem);
+  printf("active servers: %d\n", repo->active_servers);
+  repo_release(repo, repo_sem);
   return 0;
 }
